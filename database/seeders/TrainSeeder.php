8b0451136be5cea54bbc72380b86bd9065ee35b4
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-05-27 17:00:40 +0200

    Copiato FarkerFactory e impostati dati in italiano (it_IT).

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -5,17 +5,17 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
+use Faker\Factory as FakerFactory; // Aggiungi questa riga per importare FakerFactory
 use App\Models\Train;
-
 
 class TrainSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    
-        public function run(Faker $faker): void
+    public function run(Faker $faker): void
     {
+        $faker = FakerFactory::create('it_IT'); 
 
         for ($i = 0; $i < 150; $i++) {
             $new_train = new Train();
@@ -23,14 +23,14 @@ class TrainSeeder extends Seeder
             $new_train->company = $faker->company();
             $new_train->departure_station = $faker->city();
             $new_train->arrival_station = $faker->city();
-            $new_train->departure_time = $faker->dateTimeBetween('-1 day'); 
+            $new_train->departure_time = $faker->dateTimeBetween('-1 day');
             $new_train->arrival_time = $faker->dateTimeBetween($new_train->departure_time, '+1 day');
             $new_train->train_code = $faker->unique()->randomNumber(5, true);
             $new_train->carriages_number = $faker->numberBetween(1, 11);
             $new_train->punctual = $faker->boolean();
             $new_train->deleted = !$new_train->punctual;
 
-            // salviamo i dati con il metodo save()
+            // Salviamo i dati con il metodo save()
             $new_train->save();
         }
     }
